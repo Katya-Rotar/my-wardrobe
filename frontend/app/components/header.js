2423618fb2d2813d84e2a8c16142bf50582db94e
Katya-Rotar <katya.rotar02@gmail.com>
2025-01-23 15:40:08 +0200

    feat(frontend): added sidebar navigation component with icons

---
+++ frontend/app/components/header.js
@@ -0,0 +1,47 @@
+import Link from "next/link"
+import Image from "next/image"
+import styles from '../styles/Header.module.css'
+
+const Header = () => {
+   return(
+      <nav className={styles.sidebar}>
+         <div className={`${styles.link} ${styles.home}`}>
+            <Link href={"/"} legacyBehavior>
+               <a>
+                  <Image src="/home.svg" alt="Home" width={20} height={20} />
+               </a>
+            </Link>
+         </div>
+         <div className={styles.link}>
+            <Link href={"/"} legacyBehavior>
+               <a>
+                  <Image src="/clothing.svg" alt="Clothing" width={20} height={20} />
+               </a>
+            </Link>
+         </div>
+         <div className={styles.link}>
+            <Link href={"/"} legacyBehavior>
+               <a>
+                  <Image src="/wardrobe.svg" alt="Wardrobe" width={18} height={18} />
+               </a>
+            </Link>
+         </div>
+         <div className={styles.link}>
+            <Link href={"/"} legacyBehavior>
+               <a>
+                  <Image src="/socialNetwork.svg" alt="Social Network" width={20} height={20} />
+               </a>
+            </Link>
+         </div>
+         <div className={styles.link}>
+            <Link href={"/"} legacyBehavior>
+               <a>
+                  <Image src="/liked.svg" alt="Liked" width={20} height={20} />
+               </a>
+            </Link>
+         </div>
+      </nav>
+   )
+}
+
+export default Header
